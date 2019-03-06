9706e5606ca498a91b6599a65f0e39cdcfe6c97f
nosu <tomasz.nosall@gmail.com>
2019-03-06 15:58:45 +0100

    Controller request mapping

--- spring-mvc-demo/src/co/tomcio/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/co/tomcio/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
@@ -31,6 +33,20 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		// create the message
 		String message = "Yo! " + theName;
+		// add message to the model
+		model.addAttribute("message", message);
+
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName , Model model) {
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		// create the message
+		String message = "Hey My Freind v3 " + theName;
 		// add message to the model
 		model.addAttribute("message", message);
 
