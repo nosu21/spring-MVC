c0b57ea6b693dd72210ad83af2e9aa170ccc086f
nosu <tomasz.nosall@gmail.com>
2019-03-08 14:49:15 +0100

    add initBinder

--- spring-mvc-demo/src/co/tomcio/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/co/tomcio/springdemo/mvc/CustomerController.java
@@ -2,15 +2,27 @@ package co.tomcio.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add an initbinder to convert trim input strings, remove leading
+	// whitespace
+	// resolve issue for validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -22,7 +34,7 @@ public class CustomerController {
 
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
-		System.out.println("Last name: | "+customer.getLastName());
+		System.out.println("Last name: | " + customer.getLastName());
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else
